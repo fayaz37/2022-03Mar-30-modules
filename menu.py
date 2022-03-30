a7d2e9e8e3cd874586f54be1930c74cb546f9127
Reuven Lerner <reuven@lerner.co.il>
2022-03-30 16:27:38 +0300

    gitautosync update 30

--- menu.py
+++ menu.py
@@ -3,3 +3,5 @@ def menu(*args):                # all arguments will be put in the tuple, "args"
         s = input('Enter choice: ').strip()
         if s in args:               # is s a valid response? If so, return it
             return s
+
+        print(f'Invalid choice {s}')
