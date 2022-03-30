0711116a62c27248b6774327212068df26aa27ed
Reuven Lerner <reuven@lerner.co.il>
2022-03-30 16:27:26 +0300

    gitautosync update 29

---
+++ menu.py
@@ -0,0 +1,5 @@
+def menu(*args):                # all arguments will be put in the tuple, "args"
+    while True:
+        s = input('Enter choice: ').strip()
+        if s in args:               # is s a valid response? If so, return it
+            return s
