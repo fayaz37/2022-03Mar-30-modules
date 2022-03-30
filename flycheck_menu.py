c057115804b62eca1abd8614101cf5bc97f6cc55
Reuven Lerner <reuven@lerner.co.il>
2022-03-30 16:47:23 +0300

    gitautosync update 45

---
+++ flycheck_menu.py
@@ -0,0 +1,7 @@
+def menu(*args):                # all arguments will be put in the tuple, "args"
+    while True:
+        s = input(f'Enter choice {args}: ').strip()
+        if s in args:               # is s a valid response? If so, return it
+            return s
+
+        print(f'Invalid choice {s}')
